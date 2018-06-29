0daf722605657bfc379f8b42d3697e5e9cbb2955
Abin Thaha <abin@qburst.com>
2018-06-29 21:01:33 +0530

    Google Indexing

--- src/scenes/Banner/index.js
+++ src/scenes/Banner/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './index.css';
 
 class Banner extends Component {
+
   	render() {
     	return (
     		<section className='even'>
@@ -14,7 +15,7 @@ class Banner extends Component {
 		      		<span className='user-image'></span>
 		      		<h1><span className='title'>Hey, I'm</span> Abin Thaha Azeez</h1>
 		      		<h2>FRONT-END ENGINEER</h2>
-		      		<a href="../../assets/Abinthaha_resume.pdf" download="Abin Thaha Azeez.pdf" className='download-resume-btn'>Download Resume</a>
+		      		<a download href='/src/assets/Abinthaha.pdf' className='download-resume-btn'>Download Resume</a>
 		      		<span className='quote'>"Yes, I'm the CSS to your HTML"</span>
 		      		<span className='small-desc'>Specializing in building everything from small business sites to rich interactive web apps.</span>
 		      	</div>
