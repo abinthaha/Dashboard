57090f7d884a7499bdd3d6b246d247eba95c7b43
Abin Thaha Azeez <Abin.Azeez@gds.ey.com>
2019-08-28 15:21:28 +0530

    Updates

---
+++ precache-manifest.5c7c3e84503ccfe25211a902c190394b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58609dcde128a17dfdb1b65b31c93ab",
+    "url": "/Dashboard/index.html"
+  },
+  {
+    "revision": "30b464e2bc95873d4582",
+    "url": "/Dashboard/static/css/main.9c237805.chunk.css"
+  },
+  {
+    "revision": "593f398260e24979c41b",
+    "url": "/Dashboard/static/js/2.9278174c.chunk.js"
+  },
+  {
+    "revision": "30b464e2bc95873d4582",
+    "url": "/Dashboard/static/js/main.1938f5f7.chunk.js"
+  },
+  {
+    "revision": "02fd8e96ff0619532f87",
+    "url": "/Dashboard/static/js/runtime~main.48b7983d.js"
+  },
+  {
+    "revision": "c07ee871f1c6eebf442a32f4bbb56f7d",
+    "url": "/Dashboard/static/media/Abin Thaha Azees - Resume.c07ee871.pdf"
+  },
+  {
+    "revision": "20a2aadd16493b8038334cf178c501be",
+    "url": "/Dashboard/static/media/banner-bg.20a2aadd.jpg"
+  },
+  {
+    "revision": "39b0a54dfb544e8f47f6196055213995",
+    "url": "/Dashboard/static/media/user-pic.39b0a54d.jpg"
+  }
+]);
